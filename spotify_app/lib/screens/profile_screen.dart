6ec3ad584b68850344a61c917ba04b2cbbf65d48
Your Name <you@example.com>
2025-05-28 16:32:01 +0200

    added code comments, and made the playlist list tiles responsive (open the playlist screen with the full detailed playlist info and track lists)

--- spotify_app/lib/screens/profile_screen.dart
+++ spotify_app/lib/screens/profile_screen.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:spotify_app/screens/playlist_screen.dart';
+import 'package:spotify_app/services/auth_service.dart';
 import 'package:spotify_app/utils/constants.dart';
 
 // Used this site to help with what I can display on the page https://developer.spotify.com/documentation/web-api/howtos/web-app-profile
-
+// A screen to display the profile of
+// the logged in user and their playlists
 class ProfileScreen extends StatelessWidget {
-  final Future<Map<String, dynamic>> profileFuture;
-  final Future<Map<String, dynamic>> playlistsFuture;
+  final Future<Map<String, dynamic>> profileFuture; // load user profile data
+  final Future<Map<String, dynamic>> playlistsFuture; // load user;s playlists
 
   const ProfileScreen({
     super.key,
@@ -16,9 +19,15 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(" ")),
+      appBar: AppBar(
+        title: Text(" "),
+        backgroundColor: black,
+        iconTheme: IconThemeData(color: white),
+      ),
       body: FutureBuilder<Map<String, dynamic>>(
         future: profileFuture,
+        // This is to indicate that the application is busy and and avoid red screen error
+        // https://api.flutter.dev/flutter/material/CircularProgressIndicator-class.html
         builder: (context, profileSnapshot) {
           if (profileSnapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
@@ -30,11 +39,13 @@ class ProfileScreen extends StatelessWidget {
 
           return FutureBuilder<Map<String, dynamic>>(
             future: playlistsFuture,
+            // This is to indicate that the application is busy and and avoid red screen error
+            // https://api.flutter.dev/flutter/material/CircularProgressIndicator-class.html
             builder: (context, playlistsSnapshot) {
               if (playlistsSnapshot.connectionState ==
                   ConnectionState.waiting) {
                 return Center(child: CircularProgressIndicator());
-              } else if (playlistsSnapshot.hasError) {
+              } else if (playlistsSnapshot.hasError) { // If something goes wrong
                 return Center(child: Text("Error: ${playlistsSnapshot.error}"));
               }
 
@@ -88,6 +99,23 @@ class ProfileScreen extends StatelessWidget {
                   ),
                   ...playlists.map((playlist) {
                     return ListTile(
+                      onTap: () async {
+                        // Load full playlist details on tap
+                        final fullPlaylist = await fetchPlaylistDetails(
+                          playlist['id'],
+                        );
+                        print(fullPlaylist);
+
+                        // Go to the PlaylistScreen with the fetched data
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder:
+                                (context) =>
+                                    PlaylistScreen(playlist: fullPlaylist),
+                          ),
+                        );
+                      },
                       leading:
                           playlist['images'].isNotEmpty
                               ? Image.network(
